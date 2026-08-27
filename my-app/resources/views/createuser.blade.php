03c2c548f15349e86e5f2eaa5d8b99cfdff7569e
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-27 16:15:41 +0530

    feat:form created

--- my-app/resources/views/createuser.blade.php
+++ my-app/resources/views/createuser.blade.php
@@ -3,4 +3,9 @@
     Create user
   </x-slot:heading>
   <h2 class="text-bold text-yellow-300">{{$name}}</h2>
+  <form action="" class="border p-2 max-w-100 mx-auto flex flex-col gap-2">
+    <input placeholder="enter name" type="text" />
+    <input placeholder="enter name" type="text" />
+    <button type="submit" class="bg-black text-white">submit</button>
+  </form>
 </x-layout>
