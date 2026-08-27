04aaf3a3d52a2fbda5badfe046d7817dd02a10c9
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-27 16:17:52 +0530

    feat:form design completed

--- my-app/resources/views/createuser.blade.php
+++ my-app/resources/views/createuser.blade.php
@@ -4,8 +4,8 @@
   </x-slot:heading>
   <h2 class="text-bold text-yellow-300">{{$name}}</h2>
   <form action="" class="border p-2 max-w-100 mx-auto flex flex-col gap-2">
-    <input placeholder="enter name" type="text" />
-    <input placeholder="enter name" type="text" />
+    <input placeholder="enter name" class="border rounded-sm p-1" type="text" />
+    <input placeholder="enter name" class="border rounded-sm p-1" type="text" />
     <button type="submit" class="bg-black text-white">submit</button>
   </form>
 </x-layout>
