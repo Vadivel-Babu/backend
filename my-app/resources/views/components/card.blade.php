37ffadb9a893b391cd09e495715c3b10d1ebad1b
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-31 23:42:01 +0530

    feat:changed the card design

--- my-app/resources/views/components/card.blade.php
+++ my-app/resources/views/components/card.blade.php
@@ -1,7 +1,6 @@
 <div class="border bg-white p-2 m-1 max-w-max">
   <img src="" class="w-10 object-cover" alt="">
   <h2>name is: <span class="font-bold text-lg">{{$name}}</span> </h2>
-  <h2> mail is: <span class="font-bold text-lg">{{$mail}}</span></h2>
   <x-btn>
     <a href="{{$url}}">see</a>
   </x-btn>
