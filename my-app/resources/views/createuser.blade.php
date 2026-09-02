d022bf82134d04169942d48707a06c575b0d0e4d
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-09-02 13:06:24 +0530

    feat:occupation field added

--- my-app/resources/views/createuser.blade.php
+++ my-app/resources/views/createuser.blade.php
@@ -5,6 +5,11 @@
   <form action="" class="border p-2 max-w-100 mx-auto flex flex-col gap-2">
     <x-input type="text" placeholder="enter your name" />
     <x-input type="email" placeholder="enter your email" />
+    <select name="occupation" id="" class="border py-1">
+      <option value="frontend developer">frontend developer</option>
+      <option value="backend developer">backend developer</option>
+      <option value="ai developer">ai developer</option>
+    </select>
     <x-btn>Submit</x-btn>
   </form>
 </x-layout>
