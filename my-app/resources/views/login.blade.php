2c08b0fb1830a7c0ecbd0b26957ed94c3ce7e591
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-09-12 10:23:36 +0530

    feat:protected routes implemented

--- my-app/resources/views/login.blade.php
+++ my-app/resources/views/login.blade.php
@@ -3,10 +3,10 @@
     login
   </x-slot:heading>
   <form action="" method="post" class="border p-2 max-w-100 mx-auto flex flex-col mt-2 gap-2">
-    <x-input type="text" value="{{old('name')}}" name="name" placeholder="enter your name" />
+
     <x-input type="email" value="{{old('mail')}}" name="mail" placeholder="enter your mail" />
     <x-input type="password" name="password" placeholder="enter your password" />
-    <x-btn color="text-white">signup</x-btn>
+    <x-btn color="text-white">login</x-btn>
     <p>
       Don't have account? <a href="/register" class="underline text-blue-500">signup</a>
     </p>
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [EmployeeController::class, 'home']);
-Route::get('/add', [EmployeeController::class, 'create'])->name('add');
-Route::post('/store', [EmployeeController::class, 'store'])->name('store');
-Route::delete('/remove/{id}', [EmployeeController::class, 'remove'])->name('remove');
-Route::get('/users', [EmployeeController::class, 'index'])->name('allusers');
-Route::put('/update/{id}', [EmployeeController::class, 'updateEmployee'])->name('update');
-Route::get('/user/{id}', [EmployeeController::class, 'getEmployee'])->name('user');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [EmployeeController::class, 'home']);
+    Route::get('/add', [EmployeeController::class, 'create'])->name('add');
+    Route::post('/store', [EmployeeController::class, 'store'])->name('store');
+    Route::delete('/remove/{id}', [EmployeeController::class, 'remove'])->name('remove');
+    Route::get('/users', [EmployeeController::class, 'index'])->name('allusers');
+    Route::put('/update/{id}', [EmployeeController::class, 'updateEmployee'])->name('update');
+    Route::get('/user/{id}', [EmployeeController::class, 'getEmployee'])->name('user');
+});
+
 Route::get('/register', [UserController::class, 'signup'])->name('register');
+Route::post('/register', [UserController::class, 'signup'])->name('user.register');
 Route::get('/login', [UserController::class, 'signin'])->name('login');
