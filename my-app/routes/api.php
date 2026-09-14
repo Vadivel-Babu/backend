0e36a9a29752a4e90865be15d9295d995ba7e21c
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-09-14 20:11:58 +0530

    feat:api end point changed

--- my-app/routes/api.php
+++ my-app/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/apiusers', [EmployeeController::class, 'home']);
