c1ae96b030033eddbde97e634a5a55149a9dc435
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-09-19 11:54:05 +0530

    feat:blogapi project iniated

---
+++ blogapi/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $role = $request->role ? $request->role : 'user';
+        $pass = Hash::make($request->password);
+        $data = ['name' => $request->name, 'role' => $role, 'email' => $request->email, 'password' => $pass];
+
+        return response()->json($data);
+    }
+}
